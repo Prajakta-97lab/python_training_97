a15911d14ef0f4fcb4b36209dabddbe6fd27b228
prajaktaacharya0907@gmail.com <prajaktaacharya0907@gmail.com>
2024-08-22 14:10:16 +0530

    p2 added

--- python/p2.py
+++ python/p2.py
@@ -10,4 +10,7 @@ my_number = int(input('Enter a number to check if it is Even or not: '))
 print(type(my_number))
 
 if my_number % 2 == 0:
+    print(my_number,'is n even number')
+else:
+    pritn(my_number'is not an even number')
     
