f12147b93b06276fbfb8d07041a9e7de541f727f
prajaktaacharya0907@gmail.com <prajaktaacharya0907@gmail.com>
2024-08-30 16:24:30 +0530

    added

---
+++ python/numpy10.py
@@ -0,0 +1,16 @@
+import numpy as np
+
+
+array1 = np.array([2,12,3,15,25,45,30,33])
+print('Array1 = \n', array1)
+
+roots = np.sqrt(array1)
+print('Roots = \n', roots)
+
+exponentials = np.exp(array1)
+print('Exponentials= \n', exponentials)
+
+print('Element at index 3=',array1[3])
+
+array2 = array1[1:5]
+print('Sliced Array =\n',array2)
